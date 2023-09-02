41e2a6ff4a769a8446c6c879e0383d8260dba13c
LuiArm <Luisarmendariz24@icloud.com>
2023-09-01 22:32:32 -0600

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by luis armendariz on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
