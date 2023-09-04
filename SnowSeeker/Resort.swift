4a2b75c8dbc97862a7b0f4d00ec75fb425647232
LuiArm <Luisarmendariz24@icloud.com>
2023-09-03 23:53:27 -0600

    Create resort struct

---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,22 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by luis armendariz on 9/3/23.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+}
